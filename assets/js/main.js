8915404ca13043c80ff5a7ab087d3ffef810c9d3
Azurior <tonygohin@ip-172-31-19-139.eu-west-3.compute.internal>
2022-11-24 14:00:32 +0000

    update 15h

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,31 @@
 
-let home = document.getElementById('#home');
-let red = document.getElementById('#red');
-let orange = document.getElementById('#orange');
+let home = document.getElementById('home');
+let red = document.getElementById('red');
+let orange = document.getElementById('orange');
 let yellow = document.getElementById('yellow');
-let green = document.getElementById('#green');
-let blue = document.getElementById('#blue');
-let purple = document.getElementById('#purple');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
+let purple = document.getElementById('purple');
 
 let positionRed = red.getBoundingClientRect().top; // Connaître la position en hauteur d'un élément
 let positionOrange = orange.getBoundingClientRect().top;
 let positionYellox = yellow.getBoundingClientRect().top;
-let postiionGreen = green.getBoundingClientRect().top;
+let positionGreen = green.getBoundingClientRect().top;
 let positionBlue = blue.getBoundingClientRect().top;
 let positionPurple = purple.getBoundingClientRect().top;
 
 let currentScrollPosition = document.documentElement.scrollTop; // Connaître la position actuel du scroll
+
+let section = document.querySelectorAll("body > main > section");
+
+console.log(positionRed);
+console.log(positionOrange);
+console.log(positionYellox);
+console.log(positionGreen);
+console.log(positionBlue);
+console.log(positionPurple);
+console.log(currentScrollPosition);
+console.log(section);
 
 
 
@@ -24,30 +35,40 @@ let currentScrollPosition = document.documentElement.scrollTop; // Connaître la
 
 // scroll(0, positionToGo);
 
-function scrollTop (e){
+function selectSectionTop (e){
     
-    if (e.id === "red"){
+    if(e.id === "red"){
         return orange;
         if(e.id === "orange"){
+            return yellow;
+            if(e.id === "yellow"){
+                return green;
+                if(e.id === "green"){
+                    return blue;
+                    if(e.id === "blue"){
+                        return purple;
+                    }
+                }
+            }
+        }
     }
+};
+
+
+
+
+
+// window.addEventListener("DOMContentLoaded", function(event){
     
-}
+//     currentScrollPosition.addEventListener("wheel", function(event){
 
-
-
-
-
-window.addEventListener("DOMContentLoaded", function(event){
-    
-    currentScrollPosition.addEventListener("wheel", function(event){
-
-        if(event.deltaY > 0) // je vais vers le bas
-        {
+//         if(event.deltaY > 0) // je vais vers le bas
+//         {
             
-        }
-        else if(event.deltaY < 0) // je vais vers le haut
-        {
+//         }
+//         else if(event.deltaY < 0) // je vais vers le haut
+//         {
         
-        }
-    });
-});
+//         }
+//     });
+// });
